7ab5c04ab9120a922133f11f5f70496cf5e7fbcb
jusk02 <juskhc@gmail.com>
2016-04-27 13:17:55 -0500

    modificada la estructura del formulario, tratando de hacerlo mas lineal y amistoso

--- app/assets/javascripts/reactive.js
+++ app/assets/javascripts/reactive.js
@@ -24,3 +24,13 @@
 	  		});
 		});   
 
+	$(function() {
+  		$('input#show_trabajador').change(function(event){
+    		if( $(this).is(':checked') ) ;
+    		event.preventDefault();
+    		console.log("hola")
+    		$('div#trabajador').toggle(true);
+    		$('div#no_trabajador').toggle(false);
+  			console.log("hola2")
+  		});
+	});   
