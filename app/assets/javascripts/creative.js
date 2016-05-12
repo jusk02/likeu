845638a94932dd577aecd7635ec5e841f568c220
jusk02 <juskhc@gmail.com>
2016-05-12 17:13:31 -0500

    parallax block on mobile

--- app/assets/javascripts/creative.js
+++ app/assets/javascripts/creative.js
@@ -69,28 +69,8 @@
     });
     // Prallax//
 
-var ismobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
-$(document).ready(function(){
 
-    
-    if (!ismobile){
- // bypass parallax effect
-    
-    $('section[data-type="background"]').each(function(){
-        var $bgobj = $(this); // assigning the object
-    
-        $(window).scroll(function() {
-            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-            
-            // Put together our final background position
-            var coords = '50% '+ yPos + 'px';
 
-            // Move the background
-            $bgobj.css({ backgroundPosition: coords });
-        }); 
-    }); 
-    }   
-});
 
 
 
