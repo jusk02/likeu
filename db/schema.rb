a06ae4a60bbdfce4c467f16b050befc48e480c81
jusk <juskhc@gmail.com>
2016-07-15 09:39:57 -0500

    confirmation corrections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160604010049) do
+ActiveRecord::Schema.define(version: 20160715143800) do
 
   create_table "clicks", force: :cascade do |t|
     t.string   "ip_address"
@@ -39,9 +39,6 @@ ActiveRecord::Schema.define(version: 20160604010049) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.string   "name"
     t.integer  "age"
     t.string   "gender"
@@ -87,8 +84,12 @@ ActiveRecord::Schema.define(version: 20160604010049) do
     t.string   "time_count"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
