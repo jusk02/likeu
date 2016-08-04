f590d3b73e8803c07f8a0f49d203dbe6043c568f
jusk <juskhc@gmail.com>
2016-08-04 13:17:48 -0500

    param receibe fixed for video test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,9 +11,39 @@ module ApplicationHelper
  
   	  def video_url
     	  @hola=request.fullpath
-        @encoded_url = request.fullpath.split("pa=")
-        if @encoded_url.length>1  
-          @encoded_url = Base64.decode64(@encoded_url)
+        @encoded_url = request.fullpath.split("p=")
+        if @encoded_url.length>1 
+          if @encoded_url[1] != nil
+           @encoded_url[1] = Base64.decode64(@encoded_url[1])
+          else
+             @encoded_url[1] = "no presente"
+          end 
+          if @encoded_url[2] != nil
+           @encoded_url[2] = Base64.decode64(@encoded_url[2])
+          else
+             @encoded_url[2] = "no presente"
+          end 
+          if @encoded_url[3] != nil
+           @encoded_url[3] = Base64.decode64(@encoded_url[3])
+          else
+             @encoded_url[3] = "no presente"
+          end 
+          if @encoded_url[4] != nil
+           @encoded_url[4] = Base64.decode64(@encoded_url[4])
+          else
+             @encoded_url[4] = "no presente"
+          end 
+          if @encoded_url[5] != nil
+           @encoded_url[5] = Base64.decode64(@encoded_url[5])
+          else
+             @encoded_url[5] = "no presente"
+          end 
+          if @encoded_url[6] != nil
+           @encoded_url[6] = Base64.decode64(@encoded_url[6])
+          else
+             @encoded_url[6] = "no presente"
+          end 
+         
           @encoded_url
         else
           @hola  
